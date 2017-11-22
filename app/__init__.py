78a8a2e163fe103f29c084136266c65afe485b5e
Yokoyama <6673423+Acrylic1322@users.noreply.github.com>
2017-11-22 15:59:52 +0900

    And API is changed. http://~~/<site>/<src>/<dst>/?text=~~~~~ ex: http://127.0.0.1/weblio/en/jp/?text=I have a pen.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import sys, json
 import urllib, urllib.request
 from app.site_parser.weblio import Weblio
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, abort
 
 app = Flask(__name__)
 @app.errorhandler(404)
@@ -12,21 +12,39 @@ def page_not_found(e):
 def hello():
     return "<h2>Hello, world!</h2>"
 
-@app.route('/weblio/', methods=['GET'])
-def weblio():
-    src = request.args.get('src')
-    if src is None:
+@app.route('/weblio/<src>/<dst>/', methods=['GET'])
+def weblio(src, dst):
+    codeset = {
+        "en": "E",
+        "jp": "J"
+    }
+
+    text = request.args.get('text')
+    if text is None:
         return ""
 
-    url = 'https://translate.weblio.jp/?lp=EJ&lpf=EJ&originalText=' + urllib.parse.quote(src)
+    src_for_url = ''
+    if src in codeset:
+        src_for_url = codeset[src]
+
+    dst_for_url = ''
+    if dst in codeset:
+        dst_for_url = codeset[dst]
+
+    if len(src_for_url) == 0 or len(dst_for_url) == 0:
+        abort(400)
+
+    translate_code = src_for_url + dst_for_url
+
+    url = 'https://translate.weblio.jp/?lp=' + translate_code + '&lpf=' + translate_code + '&originalText=' + urllib.parse.quote(text)
     req = urllib.request.Request(url=url)
     res = urllib.request.urlopen(req)
 
-    weblio = Weblio()
+    weblio = Weblio(dst)
     weblio.feed(res.read().decode('utf-8'))
     weblio.close()
 
-    return json.dumps(weblio.result, ensure_ascii=False)
+    return json.dumps(weblio.results, ensure_ascii=False)
 
 
 if __name__ == '__main__':
--- app/site_parser/weblio.py
+++ app/site_parser/weblio.py
@@ -1,26 +1,44 @@
 from app.site_parser.site_parser import SiteParser
 
 class Weblio(SiteParser):
-    def __init__(self):
+    def __init__(self, result_mode):
         SiteParser.__init__(self)
-        self.is_result_parent = False
-        self.is_result = False
-        self.result = []
+        self.is_in_result_tag = False
+        self.is_recording = False
+
+        self.results = []
+        self.one_result = ''
+
+        # This records the language of result.
+        self.result_mode = result_mode
 
     def handle_starttag(self, tag, attrs):
-        if self.is_result_parent == True and tag == 'span':
-            self.is_result = True
-            self.is_result_parent = False
+        # If the flow is going throught the li tag of result.
+        if self.is_in_result_tag and tag == 'span':
+            if self.result_mode == 'jp':
+                self.is_recording = True
+
+            else:
+                for one_attr in attrs:
+                    if 'class' in one_attr and one_attr[1] == 'popupW':
+                        self.is_recording = True
+
+        # This searches the li tag of result.
         else:
             for one_attr in attrs:
                 if 'class' in one_attr and one_attr[1] == 'translatedTextAreaLn':
-                    self.is_result_parent = True
+                    self.is_in_result_tag = True
 
     def handle_data(self, data):
-        if self.is_result:
-            self.result.append(data)
-            self.is_result = False
+        if self.is_recording:
+            self.one_result += data + ' '
+            self.is_recording = False
+
+
 
     def handle_endtag(self, tag):
-        if self.is_result_parent and tag == 'li':
-            self.is_result_parent = False
+        if self.is_in_result_tag and tag == 'li':
+            self.results.append(self.one_result.rstrip(' '))
+            self.one_result = ''
+            self.is_recording = False
+            self.is_in_result_tag = False
