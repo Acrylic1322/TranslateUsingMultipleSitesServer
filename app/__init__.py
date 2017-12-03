5cd7b0bc346256c25739e0097b9be3ae3e1b9877
Yokoyama <6673423+Acrylic1322@users.noreply.github.com>
2017-12-03 18:41:36 +0900

    Support Excite translation.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,67 @@
 import sys, json
 import urllib, urllib.request
 from app.site_parser.weblio import Weblio
-from flask import Flask, render_template, send_from_directory, request, abort
+from app.site_parser.excite import Excite
+from flask import Flask, render_template, send_from_directory, request, abort, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h2>Hello, world!</h2>"
+    res = make_response("<h2>Hello, world!</h2>")
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    return res
+
+@app.route('/excite/<src>/<dst>/', methods=['GET'])
+def excite(src, dst):
+    codeset = {
+        "en": "EN",
+        "jp": "JA"
+    }
+
+    foreign_language_set = {
+        "en": 'english'
+    }
+
+    if src == dst:
+        abort(400)
+    elif not src in codeset or not dst in codeset:
+        abort(501)
+
+    text = request.args.get('text')
+    if text is None:
+        return ""
+
+    foreign_language_for_url = ''
+
+    src_for_url = ''
+    if src in codeset:
+        src_for_url = codeset[src]
+
+        if dst != 'jp':
+            foreign_language_for_url = foreign_language_set[dst]
+
+    dst_for_url = ''
+    if dst in codeset:
+        dst_for_url = codeset[dst]
+
+        if dst != 'jp':
+            foreign_language_for_url = foreign_language_set[dst]
+
+    translate_code = src_for_url + dst_for_url
+
+    url = 'https://translate.weblio.jp/?lp=' + translate_code + '&lpf=' + translate_code + '&originalText=' + urllib.parse.quote(text)
+    url = 'http://www.excite.co.jp/world/' + foreign_language_for_url + '/?before=' + urllib.parse.quote(text) + '&wb_lp=' + translate_code
+    req = urllib.request.Request(url=url)
+    res = urllib.request.urlopen(req)
+
+    excite = Excite()
+    excite.feed(res.read().decode('utf-8'))
+    excite.close()
+
+    res = make_response(json.dumps(excite.results, ensure_ascii=False))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    return res
 
 @app.route('/weblio/<src>/<dst>/', methods=['GET'])
 def weblio(src, dst):
@@ -43,8 +97,9 @@ def weblio(src, dst):
     weblio.feed(res.read().decode('utf-8'))
     weblio.close()
 
-    return json.dumps(weblio.results, ensure_ascii=False)
-
+    res = make_response(json.dumps(weblio.results, ensure_ascii=False))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    return res
 
 if __name__ == '__main__':
     app.debug = True
